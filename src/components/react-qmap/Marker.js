dc2f734811b7f7008f381af6c3dbe82d256b6a3c
lichenbuliren <617106268@qq.com>
2018-06-24 19:06:55 +0800

    feat(Marker && MarkerList): 添加 MarkerDecoration 配置

--- src/components/react-qmap/Marker.js
+++ src/components/react-qmap/Marker.js
@@ -7,7 +7,10 @@ export default class Marker extends React.Component {
   static defaultProps = {
     position: {},
     animation: ANIMATION_DROP,
-    onClick: () => {}
+    onClick: () => {},
+    decorationNum: 1,
+    showDecoration: false,
+    visible: true
   }
 
   static propTypes = {
@@ -21,7 +24,10 @@ export default class Marker extends React.Component {
       qq.maps.MarkerAnimation.UP,
       qq.maps.MarkerAnimation.DOWN
     ]),
-    onClose: PropTypes.func
+    decorationNum: PropTypes.number,
+    onClose: PropTypes.func,
+    showDecoration: PropTypes.bool,
+    visible: PropTypes.bool
   }
 
   componentDidMount () {
@@ -33,24 +39,35 @@ export default class Marker extends React.Component {
   }
 
   initMarker = () => {
-    const { map, position: { lat, lng }, animation, onClick } = this.props
+    const {
+      map,
+      position: { lat, lng },
+      animation,
+      onClick,
+      showDecoration,
+      decorationNum = 1,
+      visible = true
+    } = this.props
     if (!map) return
-    if (!this.marker) {
-      qq.maps.event.addListenerOnce(map, 'tilesloaded', () => {
-        this.marker = new qq.maps.Marker({
-          position: new qq.maps.LatLng(lat, lng),
-          animation,
-          map
-        })
+    qq.maps.event.addListenerOnce(map, 'tilesloaded', () => {
+      const config = {
+        position: new qq.maps.LatLng(lat, lng),
+        animation
+      }
 
+      if (showDecoration) config.decoration = new qq.maps.MarkerDecoration(decorationNum, new qq.maps.Point(0, -5))
+      if (!this.marker) {
+        this.marker = new qq.maps.Marker(config)
         qq.maps.event.addListener(this.marker, 'click', () => {
           onClick({
             lat,
             lng
           })
         })
-      })
-    }
+      }
+
+      visible ? this.marker.setMap(map) : this.marker.setMap(null)
+    })
   }
 
   render () {
--- src/components/react-qmap/MarkerList.js
+++ src/components/react-qmap/MarkerList.js
@@ -5,22 +5,34 @@ import Marker from './Marker'
 export default class MarkerList extends React.Component {
   static defaultProps = {
     list: [],
-    onClick: () => {}
+    onClick: () => {},
+    showDecoration: true
   }
 
   static propTypes = {
-    list: PropTypes.arrayOf(PropTypes.shape({
-      lat: PropTypes.number,
-      lng: PropTypes.number
-    })),
+    list: PropTypes.arrayOf(
+      PropTypes.shape({
+        lat: PropTypes.number,
+        lng: PropTypes.number
+      })
+    ),
     onClose: PropTypes.func
   }
 
   render () {
-    const { list, map, onClick } = this.props
+    const { list, map, onClick, showDecoration = true } = this.props
     if (!map) return null
     return list.map((item, i) => {
-      return <Marker key={i} position={item} map={map} onClick={position => onClick(position, i)} />
+      return (
+        <Marker
+          key={i}
+          showDecoration={showDecoration}
+          decorationNum={i + 1}
+          position={item}
+          map={map}
+          onClick={position => onClick(position, i)}
+        />
+      )
     })
   }
 }
