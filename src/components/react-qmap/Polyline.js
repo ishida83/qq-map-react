be5414cc094ec34e7024db05cc44ccdfad388920
lichenbuliren <617106268@qq.com>
2018-06-24 17:40:39 +0800

    feat(Polyline): 新增折线组件

---
+++ src/components/react-qmap/Polyline.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Polyline extends React.Component {
+  static defaultProps = {
+    paths: [],
+    options: {}
+  }
+
+  render () {
+    return null
+  }
+}
